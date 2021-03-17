a200e75a7070b82423af8c14edb8c1e71a0c0811
Julia <ju-at@yandex.ru>
2021-03-18 00:04:48 +0300

    Распараллеливание

---
+++ parallel.py
@@ -0,0 +1,31 @@
+import random
+import time
+import sys
+import multiprocess
+from matplotlib import pyplot as pp
+
+
+def sum_randoms(n):
+    ran = random.Random()
+
+    item = 0
+    for i in range(n):
+        item += ran.random()
+
+    return item
+
+
+def test_all(pool):
+    l = pool.map(sum_randoms, [10000] * 50)
+    pp.plot(l)
+    pp.show()
+    return l
+
+
+if __name__ == '__main__':
+    pool = multiprocess.Pool()
+    t0 = time.time()
+    print(test_all(pool))
+    print("Time spent:", time.time() - t0)
+else:
+    print(__name__)
